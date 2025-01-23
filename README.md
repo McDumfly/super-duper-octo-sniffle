6101b9d1a044a45b0889d38d3e0febb968629bf9
Vapetlics Olivér András <vapetlics.oliver@mathiasz.local>
2025-01-23 09:17:57 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# super-duper-octo-sniffle
+# Miért érdemes tudatosan étkezni?
+
+## Hogy egyél, hogy jól legyél?
+
+Sokszor vagy fáradt, nincs energiád, nem tudsz figyelni, vagy épp túlpörögsz? Teszteld magad, és hamarosan kiderül, hogy kellene étkezned ahhoz, hogy megtapasztald a jobb, egészségesebb önmagad.  
+(A teszt nem helyettesít semmilyen alapos orvosi kivizsgálást, csupán útmutatásként szolgál az egészségesebb és környezettudatosabb táplálkozáshoz)
+
+---
+
+## Légy tudatos, egy életünk van!
+
+Javasoljuk, hogy a gyorsan felszívódó szénhidrátokat (vagy épp a szupergyorsan felszívódókat) -mint a nassok, sütemények, krumpli, rizs- cseréld lassan felszívódó szénhidrátokra:
+
+- **Basmati rizs**
+- **Hajdina**
+- **Köles**
+- **Kuszkusz**
+
+És fogyassz elég folyadékot!
+
+![Health Gif](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXV6NXFicXZnMG91dWs5aHJtNmFzM29yOG1qZWgyMDExa3FseWU3dyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/RvGJFfFGVzjgU0TT4P/giphy.gif)
+
+Minden nap legalább egy 4km-es távot sétálj le gyorssétával. Ha azt érzed, hogy nehézkes az alvás, akkor lefekvés előtt egy 30 perccel már ne nézz tv-t és ne használd a telefonodat sem. Így nyugodtabb lesz az alvásod és másnap nem kelsz fáradtan, ami miatt amúgy összezabálsz mindent.
+
+---
+
+> **„Egy egészséges testben egészséges lélek lakik.”**  
+> - Ősi mondás
+
+---
+
+[Link az egészséges étkezéshez](https://www.healthline.com/nutrition/healthy-eating-tips)
+
+---
+
+### Kezdd el most az egészséges életmódot!
+
+[![Kezdj most](https://img.shields.io/badge/Kezdd_el_most-orange)](https://www.healthline.com/nutrition/healthy-eating-tips)
---
+++ main.py
@@ -0,0 +1,68 @@
+class Convert:
+    def __init__(self, line):
+        aksjdh = line.strip().split(";")
+        self.id = aksjdh[0]
+        if aksjdh[1] == "k":
+            self.kerdes = aksjdh[2]
+            self.valaszok = []
+        elif aksjdh[1] == "v":
+            try:
+                pont = int(aksjdh[3])
+            except ValueError:
+                pont = 0
+
+            self.valaszok.append({"valasz": aksjdh[2], "pont": pont})
+
+kerdesek = []
+current_kerdes_valasz = None
+
+f = open("kerdesek.csv", "r", encoding="utf-8")
+for i in f:
+        parts = i.strip().split(";")
+        if parts[1] == "k":
+            if current_kerdes_valasz:
+                kerdesek.append(current_kerdes_valasz)
+            current_kerdes_valasz = Convert(i)
+        elif parts[1] == "v" and current_kerdes_valasz:
+            try:
+                pont = int(parts[3])
+            except ValueError:
+                pont = 0
+            current_kerdes_valasz.valaszok.append({"valasz": parts[2], "pont": pont})
+
+if current_kerdes_valasz:
+    kerdesek.append(current_kerdes_valasz)
+f.close()
+
+pontszam = 0
+question_number = 1
+
+for i in kerdesek:
+    print(f"{question_number}. Kérdés: {i.kerdes}\n")
+
+    for j in range(len(i.valaszok)):
+        print(f"{j + 1}) {i.valaszok[j]['valasz']}")
+
+    valasz_szam = input("\nVálassz egy számot (1, 2 vagy 3): ").strip()
+
+    while not valasz_szam.isnumeric():
+        print("Hiba: csak számot tudsz beírni.")
+        valasz_szam = input("\nVálassz egy számot (1, 2 vagy 3): ").strip()
+
+    while int(valasz_szam) not in [1, 2, 3]:
+        print("Hiba: csak 1, 2 vagy 3 válaszok közül választhatsz.")
+        valasz_szam = input("\nVálassz egy számot (1, 2 vagy 3): ").strip()
+
+    valasz_szam = int(valasz_szam)
+
+    pontszam += int(i.valaszok[valasz_szam - 1]["pont"])
+
+    question_number += 1
+
+print(f"Pontszám: {pontszam}")
+if 21 >= pontszam >= 13:
+    print("Gratulálunk, te tudod, hogy kell igazán egészségesen élni. Ami nagyon fontos, hogy továbbra is figyelj oda a megfelelő hidratálásra és a rostbevitelre. Ha még nem próbáltad, akkor itt az ideje kipróbálni az alternatív fehérje megoldásokat is. Szuper egészséges és finom tud lenni. Egyre vigyázz, azért ne hajtsd túl magad. ;)")
+elif 30 >= pontszam >= 22:
+    print("Jó úton jársz, de még van mit javítani az étkezéseden. Figyelj a rost és a megfelelő fehérje bevitelre (hal, pulyka vagy csirke legyen a fő és a hüvelyes zöldségek). Nézz utána a mediterrán étrendnek, a tested meg fogja hálálni. A nassolást, amennyire lehet, mellőzd. A nyugodt alváshoz pedig elengedhetetlen a jó környezet, a sötét szoba. Nyugi, nincs szörny az ágy alatt. ;)")
+elif 39 >= pontszam >= 31:
+    print("Ajjaj, nagy a baj. Nem figyelsz az étkezésedre. Ha ezen nem változtatsz, komoly egészségügyi következményei is lehetnek (mint a cukorbetegség, a magas vérnyomás vagy a korai csontritkulás).")
